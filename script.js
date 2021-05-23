18128f482d4458cd49d7ae694b1d7f3aa1322b5f
TTABG <84647401+TTABG@users.noreply.github.com>
2021-05-22 17:12:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ s.addEventListener("click", function(){
     if(score < 7){
         t = "Not an ABG<br><br>You do not exibhit any ABG esque traits, so either not an ABG or just really REALLY good at hiding it"
     }
-    else if(score > 7 && score < 13){
+    else if(score >= 7 && score < 13){
         t = "Fun Zone<br><br>You're average ABG. You can hang out and meet with these ABG's as you're probably not crazy"
     }
-    else if(score > 13 && score < 20){
+    else if(score >= 13 && score < 20){
         t = "Date Zone<br><br>You're an ABG that can be introduced to friends, family, anyone. Straight up ABG"
     }
-    else if(score > 20 && score < 24){
+    else if(score >= 20 && score < 24){
         t = "Wifey Zone<br><br> You're someone to be considered for the long term"
     }
     else if(score >= 24){
