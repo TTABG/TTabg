39b601b618bbb5b832299073dd857fd1a4959c5c
TTABG <84647401+TTABG@users.noreply.github.com>
2021-05-22 17:58:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,19 +10,19 @@ s.addEventListener("click", function(){
     sc.setAttribute("style", "text-align: center; font-size: 30px;");
     score += document.querySelectorAll('input[type="checkbox"]:checked').length;
     var t = "";
-    if(score < 7){
+    if(score < 16){
         t = "Not an ABG<br><br>You do not exibhit any ABG esque traits, so either not an ABG or just really REALLY good at hiding it"
     }
-    else if(score >= 7 && score < 13){
+    else if(score >= 16 && score <20 ){
         t = "Fun Zone<br><br>You're average ABG. You can hang out and meet with these ABG's as you're probably not crazy"
     }
-    else if(score >= 13 && score < 20){
+    else if(score >= 20 && score < 23){
         t = "Date Zone<br><br>You're an ABG that can be introduced to friends, family, anyone. Straight up ABG"
     }
-    else if(score >= 20 && score < 24){
+    else if(score >= 23 && score < 26){
         t = "Wifey Zone<br><br> You're someone to be considered for the long term"
     }
-    else if(score >= 24){
+    else if(score >= 26){
         t = "Unicorn<br><br>You dont exist WTF"
     }
     sc.innerHTML= score + "<br>" + t;
